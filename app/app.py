f70c1f27892aaeb7e252e8bdcd881610d3afb728
Nathan Barnes <nbarnes@azahner.com>
2022-08-30 16:53:17 -0500

    path

--- app/app.py
+++ app/app.py
@@ -45,7 +45,9 @@ hops = hs.Hops(app)
 UPLOAD_FOLDER = 'uploads' #'C:\Users\nbarnes\Documents\GitHub\pdf-linking\app\app.py'
 # UPLOADS_PATH = join(dirname(realpath(__file__)), UPLOAD_FOLDER)
 # UPLOADS_PATH = join(app.root_path, UPLOAD_FOLDER)
-UPLOADS_PATH = os.path.abspath('uploads')
+# UPLOADS_PATH = os.path.abspath('uploads')
+UPLOADS_PATH = './uploads'
+
 print ('UPLOADS_PATH', UPLOADS_PATH, os.path.abspath('uploads'), app.root_path, )
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
