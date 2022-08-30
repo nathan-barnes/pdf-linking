7815a824476c4d92eedcc86fe6325cc280542140
Nathan Barnes <nbarnes@azahner.com>
2022-08-30 17:38:49 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -206,6 +206,7 @@ def processPdf(savedFilePath):
     print('imported file', savedFilePath)
     urlpdfLink = app.config['UPLOAD_FILE']
     print ('processPdf Path', os.path.abspath('uploads'))
+    print('file test', os.path.isfile(urlpdfLink) )
 
     # for each in os.listdir(os.path.abspath('uploads')):
     #     print(each)
