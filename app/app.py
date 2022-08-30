6a97e72e61e5a4569fe571eae42538ca99e55b7f
Nathan Barnes <nbarnes@azahner.com>
2022-08-30 17:51:33 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -49,7 +49,7 @@ UPLOADS_PATH = join(app.root_path, UPLOAD_FOLDER)
 # UPLOADS_PATH = os.path.abspath('uploads')
 # UPLOADS_PATH = join(os.path.abspath(__file__), UPLOAD_FOLDER)
 # UPLOADS_PATH = './uploads'
-os.makedirs(UPLOADS_PATH, exists_ok=True)
+os.makedirs(UPLOADS_PATH, True)
 
 print ('UPLOADS_PATH', UPLOADS_PATH, os.path.abspath('uploads'), app.root_path, )
 
